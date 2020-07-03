76bf8e444e51de949e7b85528b7bb506ce66bb21
kira924age <kira924age@gmail.com>
2020-07-04 05:04:58 +0900

    edit anime.js

--- pages/anime.js
+++ pages/anime.js
@@ -11,38 +11,38 @@ export default function FavoriteAnime() {
       <h2>My favorite Anime</h2>
       Following list shows my favorite anime.
       <ul>
-        <li key="1">Fate/Zero</li>
-        <li key="2">空の境界 (Kara no Kyoukai)</li>
-        <li key="3">コードギアス (Code Geass)</li>
-        <li key="4">少女終末旅行 (Shoujo Shuumatsu Ryokou)</li>
-        <li key="5">SHIROBAKO</li>
+        <li key="1">空の境界 (Kara no Kyoukai)</li>
+        <li key="2">氷菓 (Hyouka)</li>
+        <li key="3">少女終末旅行 (Shoujo Shuumatsu Ryokou)</li>
+        <li key="4">幸腹グラフィティ (Koufuku Graffiti)</li>
+        <li key="5">コードギアス (Code Geass)</li>
         <li key="6">私に天使が舞い降りた! (Watashi ni Tenshi ga Maiorita!)</li>
-        <li key="7">
+        <li key="7">Fate/Zero</li>
+        <li key="8">SHIROBAKO</li>
+        <li key="9">
           秒速5センチメートル (5 Centimeters per Second a chain of short stories
           about their distance)
         </li>
-        <li key="8">氷菓 (Hyouka)</li>
-        <li key="9">STEINS;GATE</li>
-        <li key="10">のんのんびより (Non Non Biyori)</li>
-        <li key="11">魔法少女まどか☆マギカ (Mahou Shoujo Madoka Magica)</li>
-        <li key="12">刀語 (Katanagatari)</li>
-        <li key="13">物語シリーズ (Monogatari Series)</li>
-        <li key="14">響け！ユーフォニアム (Sound! Euphonium)</li>
-        <li key="15">とある科学の超電磁砲 (Toaru Kagaku no Railgun)</li>
-        <li key="16">さくら荘のペットな彼女 (The Pet Girl of Sakurasou)</li>
-        <li key="17">恋する小惑星 (Koisuru Asteroid)</li>
-        <li key="18">うたわれるもの (Utawarerumono)</li>
-        <li key="19">
+        <li key="10">STEINS;GATE</li>
+        <li key="11">のんのんびより (Non Non Biyori)</li>
+        <li key="12">魔法少女まどか☆マギカ (Mahou Shoujo Madoka Magica)</li>
+        <li key="13">刀語 (Katanagatari)</li>
+        <li key="14">物語シリーズ (Monogatari Series)</li>
+        <li key="15">響け！ユーフォニアム (Sound! Euphonium)</li>
+        <li key="16">とある科学の超電磁砲 (Toaru Kagaku no Railgun)</li>
+        <li key="17">さくら荘のペットな彼女 (The Pet Girl of Sakurasou)</li>
+        <li key="18">恋する小惑星 (Koisuru Asteroid)</li>
+        <li key="19">うたわれるもの (Utawarerumono)</li>
+        <li key="20">
           ドラえもん のび太と雲の王国 (Doraemon Movie 13: Nobita to Kumo no
           Oukoku)
         </li>
-        <li key="20">
+        <li key="21">
           クレヨンしんちゃん 嵐を呼ぶ モーレツ!オトナ帝国の逆襲 (Eiga Crayon
           Shin-chan: Arashi wo Yobu Mouretsu! Otona Teikoku Gyakushuu)
         </li>
-        <li key="21">魔女の宅急便 (Majo no Takkyuubin)</li>
-        <li key="22">もののけ姫 (Mononoke Hime)</li>
-        <li key="23">幸腹グラフィティ (Koufuku Graffiti)</li>
+        <li key="22">魔女の宅急便 (Majo no Takkyuubin)</li>
+        <li key="23">もののけ姫 (Mononoke Hime)</li>
         <li key="24">三ツ星カラーズ (Mitsuboshi Colors)</li>
         <li key="25">
           キノの旅 -the Beautiful World- the Animated Series (Kino's Journey
@@ -59,6 +59,10 @@ export default function FavoriteAnime() {
         <li key="29">聲の形 (Koe no Katachi)</li>
         <li key="30">ガールズ&パンツァー (Girls & Panzer)</li>
         <li key="31">はねバド！ (Hanebado!)</li>
+        <li key="32">ゆるキャン△ (Yuru Camp)</li>
+        <li key="33">ハクメイとミコチ (Hakumei to Mikochi)</li>
+        <li key="34">東京マグニチュード8.0 (Tokyo Magnitude 8.0)</li>
+        <li key="35">NEW GAME!</li>
       </ul>
     </Layout>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
-import Head from 'next/head'
-import Link from 'next/link'
-import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
+import Head from "next/head";
+import Link from "next/link";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
 
 export default function Home() {
   return (
@@ -10,7 +10,9 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello. I'm an anime otaku. I'm a student at The University of Electro-Communications.
+        <p>
+          Hello. I'm an anime otaku. I'm a student at The University of
+          Electro-Communications.
         </p>
       </section>
 
@@ -22,28 +24,45 @@ export default function Home() {
 
       <h3>My favorite Anime</h3>
       <ul>
-        <li key="1">Fate/Zero</li>
-        <li key="2">空の境界 (Kara no Kyoukai)</li>
-        <li key="3">コードギアス (Code Geass)</li>
-        <li key="4">少女終末旅行 (Shoujo Shuumatsu Ryokou)</li>
-        <li key="5">SHIROBAKO</li>
+        <li key="1">空の境界 (Kara no Kyoukai)</li>
+        <li key="2">氷菓 (Hyouka)</li>
+        <li key="3">少女終末旅行 (Shoujo Shuumatsu Ryokou)</li>
+        <li key="4">幸腹グラフィティ (Koufuku Graffiti)</li>
+        <li key="5">コードギアス (Code Geass)</li>
 
-        <Link href = "anime"><a>read more</a></Link>
+        <Link href="anime">
+          <a>read more</a>
+        </Link>
       </ul>
 
       <h3>Links</h3>
       <ul>
-        <li key="1">Twitter: <a href="https://twitter.com/kira924age">@kira924age</a></li>
-        <li key="2">Github: <a href="https://github.com/kira924age">kira924age</a></li>
-        <li key="3">Hatena Blog1: <a href="https://kira000.hatenadiary.jp/">kira924ageの雑記帳</a></li>
-        <li key="4">Hatena Blog2: <a href="https://kira924age.hatenadiary.com/">kira924age’s note</a></li>
+        <li key="1">
+          Twitter: <a href="https://twitter.com/kira924age">@kira924age</a>
+        </li>
+        <li key="2">
+          Github: <a href="https://github.com/kira924age">kira924age</a>
+        </li>
+        <li key="3">
+          Hatena Blog1:{" "}
+          <a href="https://kira000.hatenadiary.jp/">kira924ageの雑記帳</a>
+        </li>
+        <li key="4">
+          Hatena Blog2:{" "}
+          <a href="https://kira924age.hatenadiary.com/">kira924age’s note</a>
+        </li>
       </ul>
 
       <h3>Programming Contest</h3>
       <ul>
-        <li key="1">AtCoder: <a href="https://atcoder.jp/users/kira924age">kira924age</a></li>
-        <li key="2">Codeforces: <a href="https://codeforces.com/profile/kira924age">kira924age</a></li>
+        <li key="1">
+          AtCoder: <a href="https://atcoder.jp/users/kira924age">kira924age</a>
+        </li>
+        <li key="2">
+          Codeforces:{" "}
+          <a href="https://codeforces.com/profile/kira924age">kira924age</a>
+        </li>
       </ul>
     </Layout>
-  )
+  );
 }
