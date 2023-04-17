27d1f3b46d78d3676ce5a83d5edfd4f351766b34
kira924age <kira924age@gmail.com>
2023-04-17 23:11:16 +0900

    add GoogleAnalytics

---
+++ src/components/common/Analytics.tsx
@@ -0,0 +1,22 @@
+import Script from "next/script";
+
+const ANALYTICS_ID = "G-3BJR3TX6CR";
+
+export const Analytics: React.FC = () => {
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=${ANALYTICS_ID}"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${ANALYTICS_ID}');
+        `}
+      </Script>
+    </>
+  );
+};
