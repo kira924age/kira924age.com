e0645fe2317cc43b6e809d3669ca094de3905192
kira924age <kira924age@gmail.com>
2023-04-30 22:06:03 +0900

    fix deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,7 @@
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 PROJECT_ROOT_DIR="$SCRIPT_DIR"/../
 
-cd $(PROJECT_ROOT_DIR)
+cd $PROJECT_ROOT_DIR
 
 sudo docker build -t kira924age/kira924age.com:latest .
 sudo docker-compose up -d
