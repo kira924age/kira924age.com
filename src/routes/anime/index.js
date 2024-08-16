20a91c279f46ebb6d52fac474ad47e542b67ca71
kira924age <kira924age@gmail.com>
2024-08-16 19:38:02 +0900

    style: 💄 add prettier

--- src/routes/anime/index.js
+++ src/routes/anime/index.js
@@ -1,23 +1,21 @@
-import { h } from 'preact';
-import { FAVORITE_ANIME_LIST } from "../../constants/anime"
+import { h } from "preact";
+import { FAVORITE_ANIME_LIST } from "../../constants/anime";
 // import { useEffect, useState } from 'preact/hooks';
 
 // Note: `user` comes from the URL, courtesy of our router
 const Anime = () => {
-	return (
-		<div>
-			<h1>Anime</h1>
-			<p>Following list shows my favorite anime</p>
+  return (
+    <div>
+      <h1>Anime</h1>
+      <p>Following list shows my favorite anime</p>
 
-    <ul>
-    { FAVORITE_ANIME_LIST.map((anime) => {
-      return (
-        <li key={anime.id}>{anime.name}</li>
-      )
-   })}
-    </ul>
-		</div>
-	);
+      <ul>
+        {FAVORITE_ANIME_LIST.map((anime) => {
+          return <li key={anime.id}>{anime.name}</li>;
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default Anime;
