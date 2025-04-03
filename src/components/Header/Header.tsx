d24d5205516ff7415483a6bf6a2d44d4127185a5
kira924age <kira924age@gmail.com>
2025-04-03 23:35:31 +0900

    refactor: 💡 remove commented out code

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -35,48 +35,7 @@ export const Header: React.FC = () => {
           </nav>
         </div>
       </header>
-      {/* Drawer を表示 */}
       <Drawer open={isOpen} onOpenChange={setIsOpen} />
     </>
   );
 };
-
-/*
-import { useState } from "react";
-import { Link } from "react-router";
-
-import { HamburgerButton } from "./HamburgerButton";
-import styles from "./Header.module.scss";
-
-export const Header = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const toggleIsOpen = () => {
-    setIsOpen((prev) => !prev);
-  };
-  return (
-    <header className={styles.header}>
-      <div className={styles.headerContainer}>
-        <div className={styles.hamburgerButtonContainer}>
-          <HamburgerButton open={isOpen} onClick={toggleIsOpen} />
-        </div>
-        <div className={styles.logo}>
-          <Link to="/">kira924age.com</Link>
-        </div>
-        <nav className={styles.nav}>
-          <ul>
-            <li>
-              <Link to="/anime">anime</Link>
-            </li>
-            <li>
-              <Link to="/manga">manga</Link>
-            </li>
-            <li>
-              <Link to="/music">music</Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </header>
-  );
-}
-*/
