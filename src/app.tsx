505040910e7debbd450f84f16449594d05303d83
kira924age <kira924age@gmail.com>
2025-04-04 23:34:02 +0900

    feat: 🎸 add Dialog.Title

--- src/app.tsx
+++ src/app.tsx
@@ -1,27 +1,12 @@
-import { Routes, Route } from "react-router";
+import { Outlet } from "react-router";
 
 import { Header } from "@/components/Header";
-import { Anime } from "@/pages/Anime";
-import { AnimeDetail } from "@/pages/AnimeDetail";
-import { Home } from "@/pages/Home";
-import { Manga } from "@/pages/Manga";
-import { Music } from "@/pages/Music";
-import { MangaDetail } from "@/pages/MangaDetail";
-
-import "./app.scss";
 
 export const App = () => (
   <>
     <Header />
     <main>
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path="/anime" element={<Anime />} />
-        <Route path="/anime/:animeId" element={<AnimeDetail />} />
-        <Route path="/manga" element={<Manga />} />
-        <Route path="/manga/:mangaId" element={<MangaDetail />} />
-        <Route path="/music" element={<Music />} />
-      </Routes>
+      <Outlet />
     </main>
   </>
 );
--- src/components/Header/Drawer.tsx
+++ src/components/Header/Drawer.tsx
@@ -1,6 +1,8 @@
 import React from "react";
+import clsx from "clsx";
+
 import * as Dialog from "@radix-ui/react-dialog";
-import { Link } from "react-router";
+import { Link, useLocation } from "react-router";
 import styles from "./Drawer.module.scss";
 
 type DrawerProps = {
@@ -9,33 +11,40 @@ type DrawerProps = {
 };
 
 export const Drawer: React.FC<DrawerProps> = ({ open, onOpenChange }) => {
+  const location = useLocation();
+
+  const isActiveHome: boolean = location.pathname === "/";
+  const isActiveAnime: boolean = location.pathname === "/anime";
+  const isActiveManga: boolean = location.pathname === "/manga";
+  const isActiveMusic: boolean = location.pathname === "/music";
+
   return (
     <Dialog.Root open={open} onOpenChange={onOpenChange}>
       <Dialog.Portal>
         <Dialog.Overlay className={styles.overlay} />
         <Dialog.Content className={styles.content}>
-          <div className={styles.siteTitle}>
+          <Dialog.Title className={styles.siteTitle}>
             <Link to="/" onClick={() => onOpenChange(false)}>
               kira924age.com
             </Link>
-          </div>
+          </Dialog.Title>
           <ul className={styles.list}>
-            <li>
+            <li className={clsx({ [styles.active]: isActiveHome })}>
               <Link to="/" onClick={() => onOpenChange(false)}>
                 Home
               </Link>
             </li>
-            <li>
+            <li className={clsx({ [styles.active]: isActiveAnime })}>
               <Link to="/anime" onClick={() => onOpenChange(false)}>
                 Anime
               </Link>
             </li>
-            <li>
+            <li className={clsx({ [styles.active]: isActiveManga })}>
               <Link to="/manga" onClick={() => onOpenChange(false)}>
                 Manga
               </Link>
             </li>
-            <li>
+            <li className={clsx({ [styles.active]: isActiveMusic })}>
               <Link to="/music" onClick={() => onOpenChange(false)}>
                 Music
               </Link>
